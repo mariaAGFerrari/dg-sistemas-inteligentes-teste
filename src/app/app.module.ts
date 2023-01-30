b0fd03ca7d406a153904c1fc8664996247d0400a
Maria Aparecida Guedes Ferrari <miaa_ferrari@hotmail.com>
2023-01-30 15:31:29 -0300

    teste1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    LoginFormComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
